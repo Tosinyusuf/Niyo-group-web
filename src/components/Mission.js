c7e935c3e851f8cf4fd35f82c23fbf0852e8c8a7
Akeiba <Akeiba@niyoenterprise.com>
2022-06-22 15:46:05 +0100

    worked on restructuring numbers and mission

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,34 +1,51 @@
 import StyledMission from "../styled/Mission.styled";
+import orangeRectangle from "../assets/orange-rectangle.svg";
+import arrow from "../assets/arrow.svg";
 
 const Mission = () => {
-    return (
-        <>
+    return (<>
         <StyledMission>
-            <h1>Styled Mission</h1>
+        <div className="mission-image-container">
+        </div>  
 
-            ////// CHANGE ALL THIS TO
-
-            <div className="mission-image-container">
-                <img src={classroomImage2} alt="our mission" className="mission-image"/>
-            </div>  
-
-
-            <div className="mission-right-container">
-                <div className="mission-text-container">
-                    <p className="mission-description"><img src={orangeRectangle} alt="symbol" className="rectangle" />Our Vision</p>
-                    <h1 className="mission-title">#1 Destination for Black DisruptHers Globally!</h1>
-                    <p className="mission-text">A nation of brands where ambitious black women build and create products that shape the face of culture</p>
-                    <button className="mission-button">Learn More <span><img src={Arrow} alt="symbol" className="arrow" /></span></button>
-                </div>
+        <div className="mission-right-container">
+            <div className="mission-text-container">
+                <p className="mission-description"><img src={orangeRectangle} alt="symbol" className="rectangle" />We are on a Mission</p>
+                <h1 className="mission-title">Changing the narrative for black female disruptors globally..</h1>
+                <p className="mission-text">To build ecosystems for ambitious black women who have a passion to build and create disruptive ideas and technologies that drive society forward. </p>
+                <button className="mission-button">Read More <span><img src={arrow} alt="symbol" className="arrow" /></span></button>
             </div>
-
-
-
-
+        </div> 
 
         </StyledMission>
         </>
     )
 }
 
-export default Mission;
+
+
+export default Mission; 
+
+
+
+/* <>
+<StyledMission>
+    <h1>Styled Mission</h1>
+
+    ////// CHANGE ALL THIS TO
+
+    <div className="mission-image-container">
+        <img src={classroomImage2} alt="our mission" className="mission-image"/>
+    </div>  
+
+
+    <div className="mission-right-container">
+        <div className="mission-text-container">
+            <p className="mission-description"><img src={orangeRectangle} alt="symbol" className="rectangle" />Our Vision</p>
+            <h1 className="mission-title">#1 Destination for Black DisruptHers Globally!</h1>
+            <p className="mission-text">A nation of brands where ambitious black women build and create products that shape the face of culture</p>
+            <button className="mission-button">Learn More <span><img src={Arrow} alt="symbol" className="arrow" /></span></button>
+        </div>
+    </div>
+</StyledMission>
+</> */
--- src/styled/Mission.styled.js
+++ src/styled/Mission.styled.js
@@ -1,12 +1,90 @@
 import styled from 'styled-components';
+import missionImage from "../assets/mission-image.svg";
 
 const StyledMission = styled.div`
-    background-color: green;
-    position: absolute;
-    width: 1440px;
-    height: 793px;
-    left: 0px;
-    top: 2068px;
+    height: 100vh;
+    witdh: 100vw;
+    display: flex;
+    flex-direction: row;
+    background-color: #070909;
+    color: #fff;
+
+
+    .mission-image-container {
+        background-image: url(${missionImage});
+        position: relative;
+        width: 50vw;
+        height: 110vh;
+        top: -60px;
+        background-size: 600px 1260px;
+        background-size: cover;
+        background-position: center;
+        z-index: 10;
+    }
+
+    .mission-right-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 50vw;
+        height: 100vh;
+    }
+    
+    .mission-description {
+        font-family: regular;
+        color: #FBAF00;
+        width: 245.5px;
+        height: 26px;
+        font-size: 14px;
+    }
+
+    .rectangle {
+        margin-right: 8%;
+        align-self: center;
+        padding: 0;
+        margin: 0 5% 0 0;
+    }
+
+    .mission-title {
+        width: 484px;
+        height: 90px;
+        align-self: center;
+        font-family: AtypDisplay-Bold;
+        font-size: 30px;
+        line-height: 36px;
+        margin-top: 12px;
+
+    }
+
+    .mission-text {
+        width: 30vw;
+        // height: 168px;
+        font-family: regular;
+        font-size: 20px;
+        line-height: 25px;
+    }
+
+    .mission-button {
+        margin: 32px 0 0 0;
+        padding: 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: flex-start;
+        color: #FBAF00;
+        background: none; 
+        cursor: pointer;
+        text-decoration: underline;
+        border: none;        
+        font-size: 19px;
+        font-family: regular;
+    }
+
+    .arrow {
+        box-sizing: border-box;
+        margin-left: 42%;
+    }
 `;
 
-export default StyledMission;
+export default StyledMission; 
