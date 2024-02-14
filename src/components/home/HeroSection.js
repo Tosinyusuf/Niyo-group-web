dc60657a615c86c2214417a0d012554defae2e7f
grace22411 <olayinkagrace112.ob@gmail.com>
2024-02-14 08:51:47 +0000

    updated file side of report

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className="hero-text-container">
         <h1 className="hero-text">
 
-        A group of disruptive technology brands that disrupts industries
+        A group of disruptive technology brands.
 
         
         </h1>
@@ -20,7 +20,7 @@ const HeroSection = () => {
 
     
         <a href="https://www.niyohairandbeauty.com/" target="_blank" rel="noreferrer"> <img src={button3} alt="NHB button" /></a>
-        <a href="https://labs.niyo.co" target="_blank" rel="noreferrer"> <img src={button2} alt="Niyo bootcamps button" /></a>
+        <a href="https://bootcamps.niyo.co" target="_blank" rel="noreferrer"> <img src={button2} alt="Niyo bootcamps button" /></a>
 
         </div>
       </div>
