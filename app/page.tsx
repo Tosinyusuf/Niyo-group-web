0a2deba89df47ff2aab8bbc808ba4757881b1d57
John Oba <obajohn75@gmail.com>
2024-02-22 14:50:55 +0100

    refactor: renamed folder structure of templates to include landing

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,13 @@
 import { split } from "@/animation/common";
 import LandingHeader from "@/components/header";
 import { navLinks } from "@/configs/landing";
-import FooterSection from "@/templates/footer";
-import HeroSection from "@/templates/hero";
-import Section2 from "@/templates/section2";
-import Section3 from "@/templates/section3";
-import Section4 from "@/templates/section4";
-import Office from "@/templates/offices";
-import Media from "@/templates/media";
+import FooterSection from "@/templates/landing/footer";
+import HeroSection from "@/templates/landing/hero";
+import Section2 from "@/templates/landing/section2";
+import Section3 from "@/templates/landing/section3";
+import Section4 from "@/templates/landing/section4";
+import Office from "@/templates/landing/offices";
+import Media from "@/templates/landing/media";
 
 import { useEffect } from "react";
 
