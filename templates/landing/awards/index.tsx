2733feeff30b3cdb9b81bd58ff59e0be2c7662d4
MAC <yusuftosco66@gmail.com>
2024-02-22 13:34:52 +0100

    added awards anime

--- templates/landing/awards/index.tsx
+++ templates/landing/awards/index.tsx
@@ -1,5 +1,5 @@
 // Awards.tsx
-import React, { FC } from "react";
+import React, { FC, useEffect } from "react";
 import Image, { StaticImageData } from "next/image";
 import { Button2, Text, Lines } from "@/components";
 import MBC from "@/assets/png/MBCC Awards.png";
@@ -10,12 +10,111 @@ import Innovation from "@/assets/png/Black White Classic Best Product Award Laur
 import Tech from "@/assets/png/Black White Classic Best Product Award Laurel Wreath Logo 4.png";
 import Business from "@/assets/png/Black White Classic Best Product Award Laurel Wreath Logo 3.png";
 import Barclays from "@/assets/png/Black White Classic Best Product Award Laurel Wreath Logo 2.png";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 interface AwardsProps {
   imageSrc: StaticImageData | string;
 }
 
 const Awards: FC = () => {
+  useEffect(() => {
+    console.log("Section");
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".awards",
+        start: "top 20%",
+        id: "timeline1", // Setting ID for the first ScrollTrigger
+      },
+    });
+    const tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".awards",
+        start: "top 50%",
+        id: "timeline2", // Setting ID for the second ScrollTrigger
+      },
+    });
+    tl2
+      .fromTo(
+        ".awards-header",
+        {
+          opacity: 0,
+          autoAlpha: 0,
+          yPercent: -20,
+          transformStyle: "preserve-3d",
+        },
+        { autoAlpha: 1, yPercent: 0, opacity: 1, ease: "easeIn" }
+      )
+      .fromTo(
+        ".award1",
+        {
+          opacity: 1,
+          autoAlpha: 0,
+          yPercent: -50,
+          transformStyle: "preserve-3d",
+          rotation: -45, // Start rotated -45 degrees for example
+          width: "10%", // Start with a smaller width
+          transformOrigin: "50% 50%",
+        },
+        {
+          autoAlpha: 1,
+          yPercent: 0,
+          opacity: 1,
+          ease: "easeOut",
+          rotation: 0, // End rotation at 0 degrees (no rotation)
+          width: "100%", // Animate to full width
+        //   duration: 1.5,
+        }
+      );
+    tl.fromTo(
+      ".nhb-desktop",
+      {
+        opacity: 0,
+        yPercent: 20,
+        duration: 2,
+      },
+      {
+        opacity: 1,
+        yPercent: 0,
+      }
+    )
+      .fromTo(
+        ".playcare-desktop",
+        {
+          opacity: 0,
+          rotate: 90,
+          scale: 2,
+        },
+        {
+          opacity: 1,
+          rotate: 0,
+          scale: 1,
+        }
+      )
+      .fromTo(
+        ".haircolor-desktop, .afrohair-desktop",
+        {
+          opacity: 0,
+          scale: 0.3,
+        },
+        {
+          opacity: 1,
+          scale: 1,
+        }
+      );
+    return () => {
+      // Kill GSAP timelines
+      tl.kill();
+      tl2.kill();
+
+      // Kill ScrollTriggers by ID
+      ScrollTrigger.getById("timeline1")?.kill();
+      ScrollTrigger.getById("timeline2")?.kill();
+    };
+  }, []);
+
   const awards: AwardsProps[] = [
     {
       imageSrc: MBC,
@@ -37,49 +136,49 @@ const Awards: FC = () => {
     },
   ];
   return (
-      <div className="text-center w-full">
-        <div className="">
-          <div className="flex items-center justify-center">
-            <div className="w-[40px] md:w-[70px]">
-              <Image
-                className="max-w-full w-full"
-                src={LeftFlower}
-                alt={`flower`}
-                width={70}
-                height={10}
-              />
-            </div>
-            <Text
-              variant="h2"
-              value="Awards & Recognition"
-              color="shades-50"
-              weight={400}
+    <div className="text-center w-full awards">
+      <div className="">
+        <div className="flex items-center justify-center awards-header">
+          <div className="w-[40px] md:w-[70px]">
+            <Image
+              className="max-w-full w-full"
+              src={LeftFlower}
+              alt={`flower`}
+              width={70}
+              height={10}
             />
-            <div className="w-[40px] md:w-[70px]">
-              <Image
-                className="max-w-full w-full"
-                src={RightFlower}
-                alt={`flower`}
-                width={70}
-                height={70}
-              />
-            </div>
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-6 justify-center items-center mt-8">
-            {awards.map((awards, idx) => (
-              <div key={idx} className="">
-                <Image
-                  className="w-full "
-                  src={awards?.imageSrc}
-                  alt={`Awards`}
-                  width={600}
-                  height={600}
-                />
-              </div>
-            ))}
+          <Text
+            variant="h2"
+            value="Awards & Recognition"
+            color="shades-50"
+            weight={400}
+          />
+          <div className="w-[40px] md:w-[70px]">
+            <Image
+              className="max-w-full w-full"
+              src={RightFlower}
+              alt={`flower`}
+              width={70}
+              height={70}
+            />
           </div>
         </div>
+        <div className="grid grid-cols-2 md:grid-cols-6 justify-center items-center mt-8">
+          {awards.map((awards, idx) => (
+            <div key={idx} className="award1">
+              <Image
+                className="w-full "
+                src={awards?.imageSrc}
+                alt={`Awards`}
+                width={600}
+                height={600}
+              />
+            </div>
+          ))}
+        </div>
       </div>
+    </div>
   );
 };
 
